4e15f1f8d27ff9ade9126762312c57c9edd9b7d4
AkatsukiXII <942020ryan@gmail.com>
2024-01-08 17:41:26 +0800

    application function with insert

--- gui.py
+++ gui.py
@@ -17,6 +17,7 @@ def add_button(parent, text, command, width=20):
     button = tk.Button(parent, text=text, command=command, width=width)
     button.pack(pady=10)
 
+   
 def open_query_window():
     query_window = open_new_window(window, "查詢資料", "250x250")
     add_button(query_window, "客戶資料", open_customer)
@@ -27,6 +28,182 @@ def open_query_window():
 def open_add_window():
     add_window = open_new_window(window, "新增資料", "250x250")
     # Add widgets for adding data here
+    add_button(add_window, "客戶資料", add_customer)
+    add_button(add_window, "商品資料", add_product)
+    add_button(add_window, "門市資料", add_store)
+    add_button(add_window, "交易資料", add_trade)
+    
+def add_customer():
+    cid = tk.StringVar
+    cname = tk.StringVar
+    cphone = tk.StringVar
+    cadd = tk.StringVar
+    func_window = open_new_window(window,"新增資料","500x500")
+    label1 = tk.Label(func_window, text="客戶編號: ")
+    label1.grid(row=0, column=0)
+    entry1 = tk.Entry(func_window, textvariable=cid)
+    entry1.grid(row=0, column=1)
+    label2 = tk.Label(func_window, text="客戶名稱: ")
+    label2.grid(row=1, column=0)
+    entry2 = tk.Entry(func_window, textvariable=cname)
+    entry2.grid(row=1, column=1)
+    label3 = tk.Label(func_window, text="手機號碼: ")
+    label3.grid(row=2, column=0)
+    entry3 = tk.Entry(func_window, textvariable=cphone)
+    entry3.grid(row=2, column=1)
+    label4 = tk.Label(func_window, text="客戶地址: ")
+    label4.grid(row=3, column=0)
+    entry4 = tk.Entry(func_window, textvariable=cadd)
+    entry4.grid(row=3, column=1)
+    #mybutton = tk.Button(func_window, text='送出', command=cus_subbutton)
+    #mybutton.grid(row=5, column=1, pady=10)
+    add_button(func_window,"送出",cus_subbutton)
+
+    
+
+
+    
+    def cus_subbutton():
+        conn = sqlite3.connect('C:/Users/94202/Desktop/Final/SQL/final.db')
+        cursor = conn.cursor()
+        id=cid.get()
+        name=cname.get()
+        phone=cphone.get()
+        add=cadd.get()
+        data=(id,name,phone,add)
+        cursor.execute("INSERT INTO customer (cid,cname,cphone,cadd) VALUES(?,?,?,?)",data)
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+def add_product():
+    cid = tk.StringVar
+    cname = tk.StringVar
+    cphone = tk.StringVar
+    cadd = tk.StringVar
+    func_window = open_new_window(window,"新增資料","500x500")
+    label1 = tk.Label(func_window, text="商品編號: ")
+    label1.grid(row=0, column=0)
+    entry1 = tk.Entry(func_window, textvariable=cid)
+    entry1.grid(row=0, column=1)
+    label2 = tk.Label(func_window, text="商品名稱: ")
+    label2.grid(row=1, column=0)
+    entry2 = tk.Entry(func_window, textvariable=cname)
+    entry2.grid(row=1, column=1)
+    label3 = tk.Label(func_window, text="商品價格: ")
+    label3.grid(row=2, column=0)
+    entry3 = tk.Entry(func_window, textvariable=cphone)
+    entry3.grid(row=2, column=1)
+    label4 = tk.Label(func_window, text="商品庫存: ")
+    label4.grid(row=3, column=0)
+    entry4 = tk.Entry(func_window, textvariable=cadd)
+    entry4.grid(row=3, column=1)
+    #mybutton = tk.Button(func_window, text='送出', command=cus_subbutton)
+    #mybutton.grid(row=5, column=1, pady=10)
+    add_button(func_window,"送出",cus_subbutton)
+
+    
+
+
+    
+    def cus_subbutton():
+        conn = sqlite3.connect('C:/Users/94202/Desktop/Final/SQL/final.db')
+        cursor = conn.cursor()
+        id=cid.get()
+        name=cname.get()
+        phone=cphone.get()
+        add=cadd.get()
+        data=(id,name,phone,add)
+        cursor.execute("INSERT INTO product (pid,pname,price,pstock) VALUES(?,?,?,?)",data)
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+def add_store():
+    cid = tk.StringVar
+    cname = tk.StringVar
+    cphone = tk.StringVar
+    cadd = tk.StringVar
+    func_window = open_new_window(window,"新增資料","500x500")
+    label1 = tk.Label(func_window, text="門市編號: ")
+    label1.grid(row=0, column=0)
+    entry1 = tk.Entry(func_window, textvariable=cid)
+    entry1.grid(row=0, column=1)
+    label2 = tk.Label(func_window, text="統一編號: ")
+    label2.grid(row=1, column=0)
+    entry2 = tk.Entry(func_window, textvariable=cname)
+    entry2.grid(row=1, column=1)
+    label3 = tk.Label(func_window, text="門市名稱: ")
+    label3.grid(row=2, column=0)
+    entry3 = tk.Entry(func_window, textvariable=cphone)
+    entry3.grid(row=2, column=1)
+    label4 = tk.Label(func_window, text="門市地址: ")
+    label4.grid(row=3, column=0)
+    entry4 = tk.Entry(func_window, textvariable=cadd)
+    entry4.grid(row=3, column=1)
+    #mybutton = tk.Button(func_window, text='送出', command=cus_subbutton)
+    #mybutton.grid(row=5, column=1, pady=10)
+    add_button(func_window,"送出",cus_subbutton)
+
+    
+
+
+    
+    def cus_subbutton():
+        conn = sqlite3.connect('C:/Users/94202/Desktop/Final/SQL/final.db')
+        cursor = conn.cursor()
+        id=cid.get()
+        name=cname.get()
+        phone=cphone.get()
+        add=cadd.get()
+        data=(id,name,phone,add)
+        cursor.execute("INSERT INTO store (sid,scode,sname,sadd) VALUES(?,?,?,?)",data)
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+def add_trade():
+    cid = tk.StringVar
+    cname = tk.StringVar
+    cphone = tk.StringVar
+    cadd = tk.StringVar
+    func_window = open_new_window(window,"新增資料","500x500")
+    label1 = tk.Label(func_window, text="訂單編號: ")
+    label1.grid(row=0, column=0)
+    entry1 = tk.Entry(func_window, textvariable=cid)
+    entry1.grid(row=0, column=1)
+    label2 = tk.Label(func_window, text="商品名稱: ")
+    label2.grid(row=1, column=0)
+    entry2 = tk.Entry(func_window, textvariable=cname)
+    entry2.grid(row=1, column=1)
+    label3 = tk.Label(func_window, text="訂單數量: ")
+    label3.grid(row=2, column=0)
+    entry3 = tk.Entry(func_window, textvariable=cphone)
+    entry3.grid(row=2, column=1)
+    label4 = tk.Label(func_window, text="商品單價: ")
+    label4.grid(row=3, column=0)
+    entry4 = tk.Entry(func_window, textvariable=cadd)
+    entry4.grid(row=3, column=1)
+    #mybutton = tk.Button(func_window, text='送出', command=cus_subbutton)
+    #mybutton.grid(row=5, column=1, pady=10)
+    add_button(func_window,"送出",cus_subbutton)
+
+    
+
+
+    
+    def cus_subbutton():
+        conn = sqlite3.connect('C:/Users/94202/Desktop/Final/SQL/final.db')
+        cursor = conn.cursor()
+        id=cid.get()
+        name=cname.get()
+        phone=cphone.get()
+        add=cadd.get()
+        data=(id,name,phone,add)
+        cursor.execute("INSERT INTO trade (tid,pid,tcount,price) VALUES(?,?,?,?)",data)
+        conn.commit()
+        cursor.close()
+        conn.close()
 
 def open_customer():
     """
